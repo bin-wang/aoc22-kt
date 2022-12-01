1c88682e93e77a4865aedddab3a8cd81cddb3ff3
Bin Wang <binwang@umass.edu>
2022-12-01 09:56:55 -0500

    Add solution to Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
+    fun getCalories(input: List<String>): List<Int> {
+        return input.split { it == "" }.map { items -> items.sumOf { it.toInt() } }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = getCalories(input)
+        return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = getCalories(input)
+        return calories.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,25 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Split Iterable into chunks by predicate
+ */
+fun <T> Iterable<T>.split(predicate: (T) -> Boolean): List<List<T>> {
+    tailrec fun helper(res: MutableList<List<T>>, current: MutableList<T>, iterator: Iterator<T>): List<List<T>> {
+        if (!iterator.hasNext()) {
+            res.add(current.toList())
+            return res.toList()
+        }
+        val x = iterator.next();
+        return if (predicate(x)) {
+            res.add(current.toList())
+            helper(res, mutableListOf(), iterator)
+        } else {
+            current.add(x)
+            helper(res, current, iterator)
+        }
+    }
+
+    return helper(mutableListOf(), mutableListOf(), this.iterator())
+}
