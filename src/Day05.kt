536a8602c2744bc93486e2ea33f53e8ab09e8928
Bin Wang <binwang@umass.edu>
2022-12-07 16:05:04 -0500

    Move global class definitions into an object

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,47 +1,49 @@
-class State(val stacks: List<List<Char>>) {
-    companion object {
-        fun fromString(input: String): State {
-            val lines = input.lines()
+object Day05 {
+    class State(val stacks: List<List<Char>>) {
+        companion object {
+            fun fromString(input: String): State {
+                val lines = input.lines()
 
-            // Get the number of stacks
-            val n = lines
-                .last()
-                .split(" ")
-                .last(String::isNotEmpty)
-                .toInt()
+                // Get the number of stacks
+                val n = lines
+                    .last()
+                    .split(" ")
+                    .last(String::isNotEmpty)
+                    .toInt()
 
-            // parse each stack
-            val stacks = (0 until n).map { i ->
-                lines
-                    .dropLast(1)
-                    .map { it.padEnd(4 * n + 3).chunked(4)[i][1] }
-                    .takeLastWhile(Char::isLetter)
-            }
+                // parse each stack
+                val stacks = (0 until n).map { i ->
+                    lines
+                        .dropLast(1)
+                        .map { it.padEnd(4 * n + 3).chunked(4)[i][1] }
+                        .takeLastWhile(Char::isLetter)
+                }
 
-            return State(stacks)
-        }
-    }
-
-    fun move(step: Step, chunked: Boolean): State {
-        val (i, from, to) = step
-        val cratesToMove = stacks[from - 1].take(i).let { if (chunked) it else it.reversed() }
-        val newStacks = stacks.mapIndexed { index, stack ->
-            when (index) {
-                from - 1 -> stack.drop(i)
-                to - 1 -> cratesToMove + stack
-                else -> stack
+                return State(stacks)
             }
         }
-        return State(newStacks)
+
+        fun move(step: Step, chunked: Boolean): State {
+            val (i, from, to) = step
+            val cratesToMove = stacks[from - 1].take(i).let { if (chunked) it else it.reversed() }
+            val newStacks = stacks.mapIndexed { index, stack ->
+                when (index) {
+                    from - 1 -> stack.drop(i)
+                    to - 1 -> cratesToMove + stack
+                    else -> stack
+                }
+            }
+            return State(newStacks)
+        }
+
+        fun getTopCratesAsString(): String =
+            stacks
+                .mapNotNull { it.firstOrNull() }
+                .joinToString(separator = "")
     }
 
-    fun getTopCratesAsString(): String =
-        stacks
-            .mapNotNull { it.firstOrNull() }
-            .joinToString(separator = "")
+    data class Step(val i: Int, val from: Int, val to: Int)
 }
-
-data class Step(val i: Int, val from: Int, val to: Int)
 
 fun main() {
     fun parseSteps(steps: List<String>) = steps.map {
@@ -49,13 +51,13 @@ fun main() {
         val i = chunks[1].toInt()
         val from = chunks[3].toInt()
         val to = chunks[5].toInt()
-        Step(i, from, to)
+        Day05.Step(i, from, to)
     }
 
     fun part1(input: String): String {
         val (initialStateStr, stepsStr) = input.split("\n\n")
         val steps = parseSteps(stepsStr.trim().lines())
-        return steps.fold(State.fromString(initialStateStr)) { state, step ->
+        return steps.fold(Day05.State.fromString(initialStateStr)) { state, step ->
             state.move(step, chunked = false)
         }.getTopCratesAsString()
     }
@@ -63,7 +65,7 @@ fun main() {
     fun part2(input: String): String {
         val (initialStateStr, stepsStr) = input.split("\n\n")
         val steps = parseSteps(stepsStr.trim().lines())
-        return steps.fold(State.fromString(initialStateStr)) { state, step ->
+        return steps.fold(Day05.State.fromString(initialStateStr)) { state, step ->
             state.move(step, chunked = true)
         }.getTopCratesAsString()
     }
